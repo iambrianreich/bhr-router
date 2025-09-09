185d3dceb619686eb4a4be1e96f041c06479b1b8
Brian Reich <brian@brianreich.dev>
2025-09-08 21:34:26 -0400

    feat: TokenizedRoute

---
+++ src/Routes/TokenizedRoute.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * This file contains BHR\$CLASS
+*/
+namespace BHR\Router\Routes;
+
+use InvalidArgumentException;
+
+/**
+ * A TokenizedRoute is a route which may specify named tokens as part of the
+ * route. When the route specifies named tokens, the values of the tokens from
+ * the most recent call to match() can be retrieved with getArguments().
+ * 
+ * Example:
+ * 
+ * $route = new TokenizedRoute('user/{id}');
+ * $route->matches('user/12'); // Returns true
+ * $route->getArguments(); // Returns ['id' => 12]
+ */
+class TokenizedRoute
+{
+    public const PATH_SEPARATOR = '/';
+    public const ERROR_INVALID_TOKEN = '"%" is an invalid url token';
+
+    /**
+     * A hash of the named arguments and their values fmro the most recent
+     * call to matches().
+     */
+    protected ?array $arguments;
+
+    /**
+     * Creates a new TokenizedRoute.
+     *
+     * @param string[] $tokens The list of tokens for the route.
+     * @throws InvalidArgumentException if any array items are not strings.
+     */
+    public function __construct(protected array $tokens) {
+        foreach ($tokens as $token) {
+            if (! is_string($token)) {
+                throw new InvalidArgumentException(sprintf(self::ERROR_INVALID_TOKEN, $token));
+            }
+        }
+
+        $this->arguments = null;
+    }
+
+    /**
+     * Creates a TokenizedRoute from a string by splitting it on the path
+     * separator ("/").
+     *
+     * Example:
+     * $route = TokenizedRoute::fromPath('user/{id}/metadata');
+     * // Equivalent to new TokenizedRoute(['user'], '{id}', 'metadata'])
+     *
+     * @param string $path The path to convert to a TokenizedRoute.
+     * @return self
+     */
+    public static function fromPath(string $path): self {
+        return new TokenizedRoute(explode(self::PATH_SEPARATOR, $path));
+    }
+
+    /**
+     * Returns true if the specified path matches the TokenizedRoute.
+     *
+     * The path matches the route if:
+     *
+     * 1. They have the same number of tokens.
+     * 2. String tokens are identical.
+     *
+     * Examples:
+     *
+     * $route = TokenizedRoute::fromString('user/{id}/groups');
+     * $route->matches('user/12/groups'); // Returns true
+     *
+     * $route = TokenizedRoute::fromString('group/{id}/metadata');
+     * $route->matches('group/12'); // Returns false
+     *
+     * @param string $path The path to test
+     * @return bool Returns true if the patch matches the TokenizedRoute
+     */
+    public function matches(string $path): bool {
+        $pathTokens = explode('/', $path);
+        $this->arguments = null;
+        $arguments = [];
+
+        if (count($pathTokens) !== count($this->tokens)) {
+            return false;
+        }
+
+        for ($index = 0; $index < count($pathTokens); $index++) {
+            $matches = [];
+            if (preg_match('/^{(.*)}$/', $this->tokens[$index], $matches)) {
+                $arguments[$matches[1]] = $pathTokens[$index];
+                continue;
+            }
+
+            if ($this->tokens[$index] !== $pathTokens[$index]) {
+                return false;
+            }
+        }
+
+        $this->arguments = $arguments;
+        return true;
+    }
+
+    /**
+     * Returns an array of values in the path that match the arguments
+     * in the TokenizedRoute's path.
+     *
+     * If not called or previous call to matches() was not successful, returns null.
+     *
+     * @return array<string,string>|null Returns a hash of arguments, or null.
+     */
+    public function getArguments(): ?array {
+        return $this->arguments;
+    }
+}
---
+++ tests/BHR/Router/Routes/TokenizedRouteTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * This file contains Tests\BHR\Router\Routes\TokenizedRouteTest
+ *
+ * Copyright $YEAR$$ Brian Reich
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the “Software”), to deal in the
+ * Software without restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+ * and to permit persons to whom the Software is furnished to do so, subject to the 
+ * following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+ * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @author Brian Reich <brian@brianreich.dev>
+ * @copyright Copyright (C) $YEAR$$ Brian Reich
+ * @license MIT
+ */
+
+declare(strict_types=1);
+
+namespace Tests\BHR\Router\Routes;
+
+use BHR\Router\Routes\TokenizedRoute;
+use PHPUnit\Framework\TestCase;
+
+class TokenizedRouteTest extends TestCase {
+    public function testClassExists(): void {
+        $this->assertTrue(class_exists(TokenizedRoute::class));
+    }
+
+    public function testMatchesFailedIfDifferentNumberOfTokens(): void {
+        $route = TokenizedRoute::fromPath('user/{userId}');
+        $path = 'user/profile/12';
+        $this->assertFalse($route->matches($path));
+    }
+
+    public function testReturnsTrueForExactMatch(): void {
+        $route = TokenizedRoute::fromPath('user/profile');
+        $this->assertTrue($route->matches('user/profile'));
+    }
+
+    public function testReturnsTrueForArgumentMatch(): void {
+        $route = TokenizedRoute::fromPath('user/{id}');
+        $this->assertTrue($route->matches('user/12'));
+        $this->assertEquals('12', $route->getArguments()['id']);
+    }
+}
